aeb923130f9a03778639a122db54f406e7aae40c
Merih Gürlek <merih@merih.eu>
2024-07-17 22:56:46 +0200

    inital commit

---
+++ core/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import {WebcomponentsComponent} from "webcomponents";
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent, RouterModule, WebcomponentsComponent],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'core';
+}

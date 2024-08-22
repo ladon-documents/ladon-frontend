ba030a943d6f62760b73d82e97284af1b55112ad
Lukas Rybacki <kontakt@icepole.de>
2024-08-22 22:36:14 +0200

    feat(LFA-16)

--- apps/core/src/app/app.component.ts
+++ apps/core/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { WebcomponentsComponent } from 'webcomponents';
-import { LadonComponent } from 'ladon-theme';
+import { LadonComponent } from '@mind/ladon-theme';
 import { AppBootstrap } from './app.bootstrap';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, WebcomponentsComponent, LadonComponent,
+  imports: [
+    NxWelcomeComponent, RouterModule, WebcomponentsComponent, LadonComponent,
     AppBootstrap
   ],
   selector: 'app-root',

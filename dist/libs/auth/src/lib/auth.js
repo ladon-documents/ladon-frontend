cf5e2503ddf66a05c5e42ec5e422caefccb4d082
Lukas Rybacki <kontakt@icepole.de>
2024-09-18 21:33:47 +0200

    refactor: remove dist

--- dist/libs/auth/src/lib/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.auth = void 0;
-function auth() {
-    return 'auth';
-}
-exports.auth = auth;
-//# sourceMappingURL=auth.js.map

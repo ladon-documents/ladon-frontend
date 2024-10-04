df29546d7ca9d9c3c3188492a10a5758d69c1d89
Lukas Rybacki <kontakt@icepole.de>
2024-10-04 21:37:26 +0200

    feat(LFA-18)

--- libs/navigation/src/lib/navigation/navigation.component.ts
+++ libs/navigation/src/lib/navigation/navigation.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'lib-navigation',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './navigation.component.html',
+	selector: "lib-navigation",
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: "./navigation.component.html",
+	styles: `
+    :host {
+      display: block;
+      height: 100%
+    }
+  `,
 })
 export class NavigationComponent {}

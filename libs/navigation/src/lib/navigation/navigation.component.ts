e4523c28390010cf57f02bda602b18f71d5f59c2
Lukas Rybacki <kontakt@icepole.de>
2024-10-11 20:58:47 +0200

    feat(LFA-18)

--- libs/navigation/src/lib/navigation/navigation.component.ts
+++ libs/navigation/src/lib/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, computed } from "@angular/core";
+import { Component, input, output, computed } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { NavigationEntry } from "../../interface/navigation-entry";
 import { NgIconComponent, provideIcons } from "@ng-icons/core";
@@ -8,6 +8,7 @@ import {
 	heroListBullet,
 	heroArrowRightStartOnRectangle,
 } from "@ng-icons/heroicons/outline";
+import { Router } from "@angular/router";
 
 @Component({
 	selector: "lib-navigation",
@@ -36,4 +37,21 @@ export class NavigationComponent {
 	navigation = input.required<NavigationEntry[]>();
 	mainMenu = computed(() => this.navigation().filter(({ type }) => type === "main"));
 	subMenu = computed(() => this.navigation().filter(({ type }) => type === "menu"));
+	navigationEntryAction = output<NavigationEntry>();
+
+	constructor(private router: Router) {}
+
+	invokeItem(item: NavigationEntry) {
+		switch (item.target) {
+			case "internal":
+				this.router.navigate([item.path]);
+				break;
+			case "action":
+				this.navigationEntryAction.emit(item);
+				break;
+			case "external":
+				window.open(item.path, "_blank");
+				break;
+		}
+	}
 }

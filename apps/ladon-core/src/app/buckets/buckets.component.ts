cf7baea516d18af424bd293b304847c16681a29f
Lukas Rybacki <kontakt@icepole.de>
2024-11-07 21:40:16 +0100

    feat(LFA-19): adding some lists styles

--- apps/ladon-core/src/app/buckets/buckets.component.ts
+++ apps/ladon-core/src/app/buckets/buckets.component.ts
@@ -1,10 +1,13 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { NgIconComponent, provideIcons } from "@ng-icons/core";
+import { heroFolder } from "@ng-icons/heroicons/outline";
 
 @Component({
 	selector: "buckets",
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, NgIconComponent],
+	providers: [provideIcons({ heroFolder })],
 	templateUrl: "./buckets.component.html",
 	styleUrl: "./buckets.component.scss",
 })

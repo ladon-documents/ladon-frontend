11cf1562a89fe0253ec6d99b2dbf3af1216dfea9
Lukas Rybacki <kontakt@icepole.de>
2025-05-14 21:53:16 +0200

    feat(LFA-22): update components markup and styles

--- ui/src/app/usermanager/users/users.component.ts
+++ ui/src/app/usermanager/users/users.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { SearchbarComponent } from '../../searchbar/searchbar.component';
 import { UsermanagerService } from '../services/usermanager.service';
 import { RouterModule } from '@angular/router';
-import { heroPlusCircle } from '@ng-icons/heroicons/outline';
+import { heroPlusCircle, heroTrash } from '@ng-icons/heroicons/outline';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { UsermanagerStore } from '../../store/usermanager.store';
 import { AliasPipe } from '@ladon/shared';
@@ -10,7 +10,7 @@ import { AliasPipe } from '@ladon/shared';
 @Component({
   selector: 'app-users',
   standalone: true,
-  providers: [provideIcons({ heroPlusCircle })],
+  providers: [provideIcons({ heroPlusCircle, heroTrash })],
   imports: [NgIconComponent, SearchbarComponent, RouterModule, AliasPipe],
   templateUrl: './users.component.html',
   styleUrls: ['../usermanager.component.scss', './users.component.scss'],

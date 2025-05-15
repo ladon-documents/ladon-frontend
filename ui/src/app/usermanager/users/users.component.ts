41e6fac5f728d6126635e8905aca4b4dd63c9a3c
Lukas Rybacki <kontakt@icepole.de>
2025-05-15 22:19:00 +0200

    feat(LFA-22): adding dialog for `POST`

--- ui/src/app/usermanager/users/users.component.ts
+++ ui/src/app/usermanager/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, ViewChild, inject, OnInit, ElementRef } from '@angular/core';
 import { SearchbarComponent } from '../../searchbar/searchbar.component';
 import { UsermanagerService } from '../services/usermanager.service';
 import { RouterModule } from '@angular/router';
@@ -6,20 +6,27 @@ import { heroPlusCircle, heroTrash } from '@ng-icons/heroicons/outline';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { UsermanagerStore } from '../../store/usermanager.store';
 import { AliasPipe } from '@ladon/shared';
+import { ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
   standalone: true,
   providers: [provideIcons({ heroPlusCircle, heroTrash })],
-  imports: [NgIconComponent, SearchbarComponent, RouterModule, AliasPipe],
+  imports: [NgIconComponent, SearchbarComponent, RouterModule, AliasPipe, ReactiveFormsModule],
   templateUrl: './users.component.html',
   styleUrls: ['../usermanager.component.scss', './users.component.scss'],
 })
 export class UsersComponent implements OnInit {
+  @ViewChild('userDialog', { static: true }) userDialog: ElementRef | undefined;
   constructor(private usermanagerService: UsermanagerService) {}
   store = inject(UsermanagerStore);
+  userAddGroup = new FormGroup({});
 
   ngOnInit(): void {
     this.store.retrieveUsers();
+  }
+
+  createUser(): void {
+    this.userDialog?.nativeElement.showModal();
   }
 }

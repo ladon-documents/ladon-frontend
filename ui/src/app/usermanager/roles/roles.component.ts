fd714d1d5baa8b38d6a2f912fdad997d37cf3a92
Lukas Rybacki <kontakt@icepole.de>
2025-06-22 22:38:26 +0200

    feat(LFA-22) add roles overview, extend `usermanager.store`

--- ui/src/app/usermanager/roles/roles.component.ts
+++ ui/src/app/usermanager/roles/roles.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import { UsermanagerStore } from '../../store/usermanager.store';
+import { FilterComponent } from '../components/filter/filter.component';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { RoleEntryModel } from '../../../api';
+import { AliasPipe, DialogComponent } from '@ladon/shared';
+import { heroPlusCircle, heroTrash } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  providers: [provideIcons({ heroPlusCircle, heroTrash })],
+  imports: [FilterComponent, NgIconComponent, DialogComponent, AliasPipe],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.scss',
 })
-export class RolesComponent {}
+export class RolesComponent implements OnInit {
+  @ViewChild(DialogComponent, { static: true }) roleDialog: DialogComponent | undefined;
+
+  filteredRoles: RoleEntryModel[] | undefined;
+  readonly store = inject(UsermanagerStore);
+
+  createRole(): void {
+    this.roleDialog?.openDialog();
+  }
+
+  ngOnInit(): void {
+    this.store.retrieveRoles();
+    this.store.loading$().subscribe((loading) => {
+      if (!loading) {
+        this.filteredRoles = this.store.roles();
+      }
+    });
+  }
+
+  onFilterTerm(term: string) {
+    this.filteredRoles = this.store
+      .roles()
+      .filter(
+        (role) =>
+          role.name?.toLowerCase().includes(term.toLowerCase()) ||
+          role.details?.toLowerCase().includes(term.toLowerCase()),
+      );
+  }
+}

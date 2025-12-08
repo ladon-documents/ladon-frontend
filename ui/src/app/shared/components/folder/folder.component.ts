36898722f039db8f43c5eb2843c7c538bd91365f
Lukas Rybacki <kontakt@icepole.de>
2025-12-08 21:14:53 +0100

    fix: remove `input` and setup variable names

--- ui/src/app/shared/components/folder/folder.component.ts
+++ ui/src/app/shared/components/folder/folder.component.ts
@@ -1,16 +1,8 @@
-import { Component, computed, input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-folder',
-  imports: [],
   templateUrl: './folder.component.html',
   styleUrl: './folder.component.scss',
 })
-export class FolderComponent {
-  fillColor = computed(() => `hsl(${this.hue()} ${this.saturation}% ${this.lightness}%)`);
-  fillColor30L = computed(() => `hsl(${this.hue()} ${this.saturation}% ${this.lightness - 20}%)`);
-  fillColor60L = computed(() => `hsl(${this.hue()} ${this.saturation}% ${this.lightness + 15}%)`);
-  hue = input<string>(window.getComputedStyle(document.body).getPropertyValue('--primary-hue').trim() || '212');
-  private readonly saturation = 80;
-  private readonly lightness = 50;
-}
+export class FolderComponent {}

94ff7eb3bf8551813e881cd09810e6b5136b94e6
Lukas Rybacki <kontakt@icepole.de>
2026-02-07 20:37:02 +0100

    feat(LFA-31): add `upload-progress` component into header

--- ui/src/app/shared/components/upload-progress/upload-progress.component.ts
+++ ui/src/app/shared/components/upload-progress/upload-progress.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { FilemanagerContentFacade } from '../../../filemanager/filemanager-content/filemanager-content.facade';
@@ -10,10 +10,15 @@ import { heroCheckCircle, heroCloudArrowUp, heroXCircle, heroXMark } from '@ng-i
   imports: [CommonModule, NgIconComponent],
   providers: [provideIcons({ heroCheckCircle, heroXCircle, heroXMark, heroCloudArrowUp })],
   templateUrl: './upload-progress.component.html',
+  styles: `
+    :host {
+      position: absolute;
+      right: 0;
+    }
+  `,
 })
 export class UploadProgressComponent {
   protected readonly filemanagerContentFacade = inject(FilemanagerContentFacade);
   protected readonly Math = Math;
-
   protected readonly uploadProgress = this.filemanagerContentFacade.uploadProgress;
 }

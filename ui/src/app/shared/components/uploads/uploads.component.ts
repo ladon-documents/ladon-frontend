ad745af23ce16179580ee1f0928043068314e468
Lukas Rybacki <kontakt@icepole.de>
2026-02-12 21:34:07 +0100

    feat(LFA-31): add `upload-progress` component into header (#55)

--- ui/src/app/shared/components/uploads/uploads.component.ts
+++ ui/src/app/shared/components/uploads/uploads.component.ts
@@ -1,13 +1,17 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { SpinnerComponent } from '../spinner/spinner.component';
+import { UploadProgressComponent } from '../upload-progress/upload-progress.component';
+import { FilemanagerContentFacade } from '../../../filemanager/filemanager-content/filemanager-content.facade';
 
 @Component({
   selector: 'lib-uploads',
-  imports: [SpinnerComponent],
+  imports: [SpinnerComponent, UploadProgressComponent],
   templateUrl: './uploads.component.html',
   styleUrl: './uploads.component.scss',
 })
 export class UploadsComponent {
-  hasActiveUploads = computed(() => (this.uploads() ?? []).length > 0);
+  protected readonly filemanagerContentFacade = inject(FilemanagerContentFacade);
+
+  hasActiveUploads = computed(() => this.filemanagerContentFacade.hasActiveUploads());
   uploads = input<string[]>();
 }

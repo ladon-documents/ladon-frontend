8de80335bc26a8eaeb63b8839258ab0e746db623
Merih Gürlek <merih@merih.eu>
2026-02-17 22:00:11 +0100

    feat: add tab nav to sidebar, fix converter service to download zip

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -5,19 +5,6 @@ export let navigationConfig: Array<any> = [];
 
 api.utility.Initalizer().initWebComponents();
 
-if (isDevMode()) {
-
-  let wcScriptElm = document.createElement("script");
-
-  wcScriptElm.setAttribute("src", './public/wc-ladon-forms.js');
-  wcScriptElm.setAttribute("type", "text/javascript");
-  wcScriptElm.setAttribute("type", "module");
-
-  document.body.appendChild(wcScriptElm);
-}
-
-
-
 fetchNavigation
   .then((res) => res.json())
   .then((nav) => (navigationConfig = nav))

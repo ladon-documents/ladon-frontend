8afb0465541795b34f14db20a554a7eaf2b5728d
Merih Gürlek <merih@merih.eu>
2026-07-10 00:02:04 +0200

    refactor: wire pluginmanager workbench container

--- ui/src/app/pluginmanager/pluginmanager.routes.ts
+++ ui/src/app/pluginmanager/pluginmanager.routes.ts
@@ -1,16 +1,13 @@
 import { Routes } from '@angular/router';
 import { PluginmanagerComponent } from './pluginmanager.component';
-import { PluginListComponent } from './plugin-list/plugin-list.component';
 
 export const pluginmanagerRoutes: Routes = [
   {
     path: '',
     component: PluginmanagerComponent,
-    children: [
-      {
-        path: ':channelName',
-        component: PluginListComponent,
-      },
-    ],
+  },
+  {
+    path: ':channelName',
+    component: PluginmanagerComponent,
   },
 ];
